0cf851f32ec435d6e5dee5c45e504d02aaea7f94
gevera <skryonline@gmail.com>
2021-02-21 17:40:44 +0500

    Fixed telephone

--- src/store/data.ts
+++ src/store/data.ts
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 
 export const siteData = writable({
   name: "TWO BROTHERS & VAN",
-  telephone: "0773 708648",
+  telephone: "+44 773 708648",
   email: "twobrothers_and_van@gmail.com",
   y: 0,
 });
