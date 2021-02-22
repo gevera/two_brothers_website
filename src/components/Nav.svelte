a33c701624c9a52ded3f4b94add3af27a7ac8b56
gevera <skryonline@gmail.com>
2021-02-22 09:41:15 +0500

    Added phone icon in navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,7 +2,6 @@
 	import { siteData, services } from "../store/data";
 
 	let open = false;
-
 </script>
 
 <style>
@@ -23,17 +22,28 @@
 			style="width:90px;height:auto;"
 			class="logo" />
 	</a>
+	<a href={`tel:${$siteData.telephone}`} class="no-desktop pr-3 mr-3">
+		<button class="e-btn circle primary inverted no-shadow no-desktop">
+			<i class="fa fa-phone fa-lg" aria-hidden="true"/>
+		</button>
+		</a>
 	<label
 		for="e-menu"
 		class="e-btn circle small plain primary no-shadow no-desktop">
-		<i class={open ? "fa fa-times" : "fas fa-bars"} />
+		<i class={open ? 'fa fa-times' : 'fas fa-bars'} />
 	</label>
 	<input type="checkbox" id="e-menu" bind:checked={open} />
 
 	<div class="e-menu narrow">
 		<i class="e-distribution" />
-		<a class="e-menu-item text-primary" href="/#hero" on:click={() => (open = false)}>Home</a>
-		<a class="e-menu-item text-primary" href="/#about" on:click={() => (open = false)}>About</a>
+		<a
+			class="e-menu-item text-primary"
+			href="/#hero"
+			on:click={() => (open = false)}>Home</a>
+		<a
+			class="e-menu-item text-primary"
+			href="/#about"
+			on:click={() => (open = false)}>About</a>
 		<div class="e-dropdown text-primary">
 			Services
 			<i class="fas fa-angle-down" />
@@ -48,10 +58,16 @@
 				<hr />
 				<a class="e-btn danger" href={`tel:${$siteData.telephone}`}>Call
 					Now</a>
-				<a class="e-btn danger" href="/#quote" on:click={() => (open = false)}>Free Quote</a>
+				<a
+					class="e-btn danger"
+					href="/#quote"
+					on:click={() => (open = false)}>Free Quote</a>
 			</div>
 		</div>
-		<a class="e-menu-item text-primary" href="/#contact" on:click={() => (open = false)}>Contacts</a>
+		<a
+			class="e-menu-item text-primary"
+			href="/#contact"
+			on:click={() => (open = false)}>Contacts</a>
 		<a
 			class="e-menu-item text-primary"
 			href={`tel:${$siteData.telephone}`}>{$siteData.telephone}</a>
